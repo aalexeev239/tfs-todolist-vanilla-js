8572aa3dc62784b1eaa260432ba7a05763858816
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2017-11-03 14:29:09 +0300

    12

--- app.js
+++ app.js
@@ -1,437 +1,212 @@
 "use strict";
 
-var listElement = document.querySelector('.list');
-var itemElementList = listElement.children;
-
-var ENTER_KEYCODE = 13;
-var templateElement = document.getElementById('todoTemplate');
-var templateContainer = 'content' in templateElement ? templateElement.content : templateElement;
-
-// сформируем задачки
-/**
- * @typedef {Object} TodoItem
- * @property {string} name - имя тудушки
- * @property {string} status - статус
- */
-
-/**
- * @type {Array.<TodoItem>}
- */
-var todoList = [
-    {
-        name: 'Позвонить в сервис',
-        status: 'todo'
-    },
-    {
-        name: 'Купить хлеб',
-        status: 'done'
-    },
-    {
-        name: 'Захватить мир',
-        status: 'todo'
-    },
-    {
-        name: 'Добавить тудушку в список',
-        status: 'todo'
-    }
-];
-
-// функция по генерации элементов
-function addTodoFromTemplate(todo) {
-    var newElement = templateContainer.querySelector('.task').cloneNode(true);
-    newElement.querySelector('.task__name').textContent = todo.name;
-    setTodoStatusClassName(newElement, todo.status === 'todo');
-
-    return newElement;
-}
-
-function setTodoStatusClassName(todo, flag) {
-    todo.classList.toggle('task_todo', flag);
-    todo.classList.toggle('task_done', !flag);
-}
-
-function onListClick(event) {
-    var target = event.target;
-    var element;
-
-    if (isStatusBtn(target)) {
-        element = target.parentNode;
-        changeTodoStatus(element);
-    }
-
-    if (isDeleteBtn(target)) {
-        element = target.parentNode;
-        deleteTodo(element);
-    }
-}
-
-function isStatusBtn(target) {
-    return target.classList.contains('task__status');
-}
-
-function isDeleteBtn(target) {
-    return target.classList.contains('task__delete-button');
-}
-
-function checkIfTodoAlreadyExists(todoName) {
-    var todoElements = listElement.querySelectorAll('.task__name');
-    var namesList = Array.prototype.map.call(todoElements, function (element) {
-        return element.textContent;
-    });
-    return namesList.indexOf(todoName) > -1;
-}
-
-function createNewTodo(name) {
-    return {
-        name: name,
-        status: 'todo'
-    }
-}
-
-// todoList
-//     .map(addTodoFromTemplate)
-//     .forEach(insertTodoElement);
-
-listElement.addEventListener('click', onListClick);
-
-var inputElement = document.querySelector('.add-task__input');
-inputElement.addEventListener('keydown', onInputKeydown);
-
-/*==================================
- =            СТАТИСТИКА            =
- ==================================*/
-
-// формируем счетчик статистики
-var stats = {
-    done: 0,
-    todo: 0
+// --- Шаг 12 Статистика ---
+const keyCode = {
+    ENTER: 13
 };
 
-// необходимые DOM элементы
-var statsElement = document.querySelector('.statistic');
-var statsDonelElement = statsElement.querySelector('.statistic__done');
-var statsTodoElement = statsElement.querySelector('.statistic__left');
-var statsTotalElement = statsElement.querySelector('.statistic__total');
+const todoStatus = {
+    TODO: 'todo',
+    DONE: 'done'
+};
 
-// создадим функции работы со статистикой
-/**
- * отрисовывает статистику в DOM
- */
-function renderStats() {
-    statsDonelElement.textContent = stats.done;
-    statsTodoElement.textContent = stats.todo;
-    statsTotalElement.textContent = stats.done + stats.todo;
-}
-
-// теперь на каждое из действий — обновление статистики
-/**
- * добавляет значение к статистике и обновляет DOM
- * @param {boolean} isTodo — статус новой тудушки
- */
-function addToStats(isTodo) {
-    if (isTodo) {
-        stats.todo++;
-    } else {
-        stats.done++;
-    }
-    renderStats();
-}
-
-/**
- * измененяет статус тудушки и обновляет DOM
- * @param {boolean} isTodo статус после изменения
- */
-function changeStats(isTodo) {
-    if (isTodo) {
-        stats.todo++;
-        stats.done--;
-    } else {
-        stats.todo--;
-        stats.done++;
-    }
-    renderStats();
-}
-
-/**
- * отрабатывает удаление тудушки и обновляет DOM
- * @param {boolean} isTodo статус удаленной тудушки
- */
-function deleteFromStats(isTodo) {
-    if (isTodo) {
-        stats.todo--;
-    } else {
-        stats.done--;
-    }
-    renderStats();
-}
-
-// теперь надо переписать старые методы, чтобы учесть статистику
-
-// /**
-//  * вставляет тудушку и обновляет статистику
-//  * @param {TodoItem} todo
-//  */
-// function insertTodoElement(todo) {
-//     var elem = addTodoFromTemplate(todo);
-//     listElement.insertBefore(elem, listElement.firstElementChild);
-//     addToStats(todo.status === 'todo');
-// }
-
-// из-за изменений в insertTodoElement чуть упростили onInputKeydown
-
-// /**
-//  * отслеживает нажатие ENTER пользователем и создает новую тудушку, если такой нет
-//  * @param {KeyboardEvent} event
-//  */
-// function onInputKeydown(event) {
-//
-//     if (event.keyCode !== ENTER_KEYCODE) {
-//         return;
-//     }
-//
-//     var todoName = inputElement.value.trim();
-//
-//     if (todoName.length === 0 || checkIfTodoAlreadyExists(todoName)) {
-//         return;
-//     }
-//
-//     var todo = createNewTodo(todoName);
-//     insertTodoElement(todo);
-//     inputElement.value = '';
-// }
-
-// /**
-//  * изменяет статус тудушки, обновляет статистику
-//  * @param {Element} element
-//  */
-// function changeTodoStatus(element) {
-//     var isTodo = element.classList.contains('task_todo');
-//     setTodoStatusClassName(element, !isTodo);
-//
-//     changeStats(!isTodo);
-// }
-
-// /**
-//  * удаляет тудушку, обновляет статистику
-//  * @param {Element} element
-//  */
-// function deleteTodo(element) {
-//     var isTodo = element.classList.contains('task_todo');
-//     listElement.removeChild(element);
-//
-//     deleteFromStats(isTodo);
-// }
-
-/*==================================
- =            ФИЛЬТРАЦИЯ            =
- ==================================*/
-
-// изменим парадигму — теперь все изменения на тудушках сначала будут отражаться на todoList
-// и лишь потом отображаться в DOM
-
-// создадим enum с возможными вариантами фильтров
-var filterValues = {
+const filter = {
     ALL: 'all',
     DONE: 'done',
     TODO: 'todo'
 };
 
-// currentFilter — текущий выбранный фильтр
-var currentFilter = filterValues.ALL;
-
-// найдем дом-элемент фильтров
-var filtersElement = document.querySelector('.filters');
-filtersElement.addEventListener('click', onFiltersClick);
-
-/**
- * обработчик клика по контейнеру с фильтрами
- * @param {MouseEvent} event
- */
-function onFiltersClick(event) {
-
-    // проверим, что кликнули по кнопке фильтра, а не куда-нибудь еще
-    var target = event.target;
-    if (!target.classList.contains('filters__item')) {
-        return;
+const todoList = [
+    {
+        name: 'Позвонить в сервис',
+        status: todoStatus.TODO
+    },
+    {
+        name: 'Купить хлеб',
+        status: todoStatus.DONE
+    },
+    {
+        name: 'Захватить мир',
+        status: todoStatus.TODO
+    },
+    {
+        name: 'Добавить тудушку в список',
+        status: todoStatus.TODO
     }
+];
 
-    // считаем значение data-filter у соответствующей кнопки
-    var value = target.dataset.filter;
+const listElement = document.querySelector('.list');
+const templateElement = document.getElementById('todoTemplate');
+const templateContainer = 'content' in templateElement ? templateElement.content : templateElement;
+const inputElement = document.querySelector('.add-task__input');
+const filterElement = document.querySelector('.filters');
+let currentFilter = filter.ALL;
 
-    // если кликнули по текущему фильтру — ничего не делаем
-    if (value === currentFilter) {
-        return;
-    }
+const statsAllElement = document.querySelector('.statistic__total');
+const statsDoneElement = document.querySelector('.statistic__done');
+const statsTodoElement = document.querySelector('.statistic__left');
 
-    // если мы дошли до этой строчки, значит надо поменять фильтр
+function getTodoElement({name, status}) {
+    const newElement = templateContainer.querySelector('.task').cloneNode(true);
+    const nameElement = newElement.querySelector('.task__name');
 
-    // уберем класс у прежней кнопки(выбранного фильтра)
-    filtersElement.querySelector('.filters__item_selected').classList.remove('filters__item_selected');
-    // и установим класс той, по которой кликнули
-    target.classList.add('filters__item_selected');
-    // изменим значение текущего выбранного фильтра
-    currentFilter = value;
-    // перерисуем список
-    renderFilteredList();
+    nameElement.textContent = name;
+    newElement.dataset.name = name;
+    setStatus(newElement, status);
+
+    return newElement;
 }
 
-/**
- * отрисовывает список в соответствии с currentFilter
- */
-function renderFilteredList() {
-    var filteredList;
-
-    // в зависимости от значения currentFilter
-    // отфильтруем список todo
-    switch (currentFilter) {
-        case filterValues.DONE:
-            filteredList = todoList.filter(function (task) {
-                return task.status === 'done';
-            });
-            break;
-
-        case filterValues.TODO:
-            filteredList = todoList.filter(function (task) {
-                return task.status === 'todo';
-            });
-            break;
-
-        default:
-            filteredList = todoList;
-            break;
-    }
-
-    // а теперь отрисуем filteredList в качестве списка тудушек
+function renderList(todos = []) {
+    // очистим список
     listElement.innerHTML = '';
-    filteredList.forEach(insertTodoElement);
+
+    const fragment = document.createDocumentFragment();
+
+    todos.forEach(todo => {
+        fragment.appendChild(getTodoElement(todo));
+    });
+
+    listElement.appendChild(fragment);
 }
 
-// теперь надо изменить все функции по работе с тудушками – они должны сохранять актуальным todoList
-// и учитывать значение фильтров
+function setStatus(todoElement, status) {
+    const isTodo = status === todoStatus.TODO;
 
-// при вводе в текстовое поле мы добавляли новую тудушку
-// 1. переработаем checkIfTodoAlreadyExists — если раньше проверку проводили на DOM элементах,
-//    подразумевая, что все элементы отображены, то теперь это может быть неверно —  надо проверять в todoList
-// 2. вынесем логику добавления в отдельную функцию
-/**
- * отслеживает нажатие ENTER пользователем и создает новую тудушку, если такой нет
- * @param {KeyboardEvent} event
- */
+    todoElement.classList.toggle('task_todo', isTodo);
+    todoElement.classList.toggle('task_done', !isTodo);
+}
+
+function changeStatus(todoElement) {
+    const index = getTodoIndexByName(todoElement.dataset.name);
+    const todo = todoList[index];
+    const isTodo = todo.status === todoStatus.TODO;
+    const newStatus = isTodo ? todoStatus.DONE : todoStatus.TODO;
+
+    todo.status = newStatus;
+}
+
+function checkTodo(todoElement) {
+    return todoElement.classList.contains('task_todo');
+}
+
+function checkStatusBtn(element) {
+    return element.classList.contains('task__status');
+}
+
+function checkDeleteBtn(element) {
+    return element.classList.contains('task__delete-button');
+}
+
+function deleteTodo(element) {
+    const index = getTodoIndexByName(element.dataset.name);
+
+    todoList.splice(index, 1);
+}
+
+function onListClick(event) {
+    const {target} = event;
+
+    if (checkStatusBtn(target)) {
+        changeStatus(target.parentElement);
+        renderFilteredList();
+        return;
+    }
+
+    if (checkDeleteBtn(target)) {
+        deleteTodo(target.parentElement);
+        renderFilteredList();
+    }
+}
+
+function getTodoIndexByName(search) {
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i].name === search) {
+            return i;
+        }
+    }
+}
+
 function onInputKeydown(event) {
-
-    if (event.keyCode !== ENTER_KEYCODE) {
+    if (event.keyCode !== keyCode.ENTER) {
         return;
     }
 
-    var todoName = inputElement.value.trim();
-
-    if (todoName.length === 0 || checkTodo(todoName)) {
+    if (!inputElement.value) {
         return;
     }
 
-    addTodo(todoName);
+    const newName = inputElement.value;
+
+    if (checkTodoExists(newName)) {
+        return;
+    }
+
+    addNewTodo(newName);
     inputElement.value = '';
 }
 
-// 1. переработаем checkIfTodoAlreadyExists — если раньше проверку проводили на DOM элементах,
-//    подразумевая, что все элементы отображены, то теперь это может быть неверно —  надо проверять в todoList
-/**
- * проверяет, существует ли тудушка с таким именем
- * @param {string} name
- * @returns {boolean}
- */
-function checkTodo(name) {
-    return !!getTodo(name);
+function checkTodoExists(newName) {
+    const elements = listElement.querySelectorAll('.task__name');
+    const names = [...elements].map(element => element.textContent);
+
+    return names.indexOf(newName) !== -1;
 }
 
-/**
- * вспомогательная функция, ищет в todoList тудушку по имени и возвращает её
- * @param todoName
- * @returns {(TodoItem|null)}
- */
-function getTodo(todoName) {
-    for (var i = 0; i < todoList.length; i++) {
-        if (todoList[i].name === todoName) {
-            return todoList[i];
-        }
+// function checkTodoExists(newName) {
+//     return todoList.some(({name}) => name === newName);
+// }
+
+function addNewTodo(name) {
+    const todo = {
+        name,
+        status: todoStatus.TODO
+    };
+
+    todoList.unshift(todo);
+    renderFilteredList();
+}
+
+function onFiltersClick({target}) {
+    const newFilter = target.dataset.filter;
+
+    if (!newFilter || newFilter === currentFilter) {
+        return;
     }
-    return null;
+
+    currentFilter = newFilter;
+    filterElement.querySelector('.filters__item_selected').classList.remove('filters__item_selected');
+    target.classList.add('filters__item_selected');
+
+    renderFilteredList();
 }
 
-// 2. вынесем логику добавления в отдельную функцию
-/**
- * создает новую тудушку, добавляет в общий список, отрисовывает при необходимости
- * @param {string} name
- */
-function addTodo(name) {
-    var newTask = createNewTodo(name);
-    todoList.push(newTask);
-    if (currentFilter !== filterValues.DONE) {
-        insertTodoElement(newTask);
-    }
-    addToStats(true);
-}
+function renderFilteredList() {
+    let todos;
 
-// обновление статистики теперь не зависит от того, вставляется ли тудушка в DOM или нет
-/**
- * вставляет тудушку и обновляет статистику
- * @param {TodoItem} todo
- */
-function insertTodoElement(todo) {
-    var elem = addTodoFromTemplate(todo);
-    listElement.insertBefore(elem, listElement.firstElementChild);
-    // addToStats(todo.status === 'todo');
-}
-
-// обновим функцию смены статуса тудушки
-// раньше было не важно, по какой тудушке кликнули. теперь надо найти эту тудушку в todoList
-// и изменить ее статус
-/**
- * измененяет статус тудушки и обновляет DOM
- * @param {boolean} isTodo статус после изменения
- */
-function changeTodoStatus(element) {
-    // извлекаем имя тудушки и находим через вспомогательную функцию
-    var task = getTodo(element.querySelector('.task__name').textContent);
-    var isTodo = task.status === 'todo';
-    // меняем статус в todoList
-    task.status = isTodo ? 'done' : 'todo';
-
-    // при фильтре "все" нужно поменять класс у тудушки, иначе удалить
-    if (currentFilter === filterValues.ALL) {
-        setTodoStatusClassName(element, !isTodo);
+    if (currentFilter === filter.ALL) {
+        todos = todoList.slice();
     } else {
-        listElement.removeChild(element);
+        const filterStatus = currentFilter === filter.TODO ? todoStatus.TODO : todoStatus.DONE;
+
+        todos = todoList.filter(({status}) => status === filterStatus);
     }
 
-    // и поменять статистику
-    changeStats(!isTodo);
+    renderList(todos);
+    updateStats();
 }
 
-// аналогично при удалении — нужно удалять из todoList
-/**
- * удаляет тудушку, обновляет статистику
- * @param {Element} element
- */
-function deleteTodo(element) {
-    var task = getTodo(element.querySelector('.task__name').textContent);
-    var isTodo = task.status === 'todo';
-    todoList.splice(todoList.indexOf(task), 1);
-    listElement.removeChild(element);
-    deleteFromStats(isTodo);
+function updateStats() {
+    const total = todoList.length;
+    const done = todoList.filter(({status}) => status === todoStatus.DONE).length;
+
+    statsAllElement.textContent = total;
+    statsDoneElement.textContent = done;
+    statsTodoElement.textContent = total - done;
 }
 
-// отрендерим первоначальный список тудушек
-todoList.forEach(insertTodoElement);
-
-// поскольку выпилили статистику из insertTodoElement,
-// нужно посчитать первоначальные значения
-var tasksDone = todoList.filter(function (item) {
-    return item.status === 'done';
-}).length;
-
-stats = {
-    done: tasksDone,
-    todo: todoList.length - tasksDone
-};
-renderStats();
+// --- Исполняемый код ---
+listElement.addEventListener('click', onListClick);
+inputElement.addEventListener('keydown', onInputKeydown);
+filterElement.addEventListener('click', onFiltersClick);
+renderFilteredList();
